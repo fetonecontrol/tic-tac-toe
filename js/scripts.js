2dcd34c552c5f7a8032e2ae182a47c8f0deaeff8
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-18 14:21:32 -0700

    create board replacement on click
    co-authored-by: Micahel Watts <wattsjmichael@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,24 @@
 function gameBoard() {
   
 }
-function WinCondition() {
-  winCondition = [];
+
+function WinCondition(){
+  this.cond1 = [1, 2, 3];
+  this.cond2 = [4, 5, 6];
+  this.cond3 = [7, 8, 9];
+  this.cond4 = [1, 4, 7];
+  this.cond5 = [1, 5, 9];
+  this.cond6 = [2, 5, 8];
+  this.cond7 = [3, 6, 9];
+  this.cond8 = [3, 5, 7];
 }
 
-function winCondition(cond1, cond2, cond3, cond4, cond5, cond6, cond7, cond8){
-  let cond1 = [1, 2, 3];
-  let cond2 = [4, 5, 6];
-  let cond3 = [7, 8, 9];
-  let cond4 = [1, 4, 7];
-  let cond5 = [1, 5, 9];
-  let cond6 = [2, 5, 8];
-  let cond7 = [3, 6, 9];
-  let cond8 = [3, 5, 7];
-}
 let winConditionX = new WinCondition();
 
 //Begin UI Logic
 $(document).ready(function() {
-  $("form#one").click(function(event) {
+  $("#one").click(function(event) {
+    $("#one").text("X");
     alert("Nice Work")
   event.preventDefault();
   });
